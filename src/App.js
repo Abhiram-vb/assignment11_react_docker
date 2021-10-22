c4ca24e63b63b01fd21ca734878bb13b3407d51f
Abhiram <pabhi2000@gmail.com>
2021-10-22 11:26:14 +0530

    added dockerfile and docker-compose.yml file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://blog.adacore.com/uploads/_2400x1200_crop_center-center_none/iStock-1144628524.jpg" className="App-logo" alt="logo" />
+        <p>
+          Welcome to Docker
+        </p>
+        <p>Hello VB</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
